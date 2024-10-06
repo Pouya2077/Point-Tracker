0a073d66508ce549d347def2dfa029a9efe6a1d8
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2024-10-06 13:25:38 -0700

    Made minor changes to the ui app.

--- src/main/ui/RestaurantApp.java
+++ src/main/ui/RestaurantApp.java
@@ -3,7 +3,12 @@ package ui;
 // Represents the ui used for the application
 public class RestaurantApp {
 
+    // runs the Restaurant app
     public RestaurantApp() {
+        runApp();
+    }
+
+    public void runApp() {
         // stub
     }
 
