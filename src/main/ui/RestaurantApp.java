ab2f09655860bc01e4cbdb376f597508dc6c7aa4
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2024-10-07 13:21:11 -0700

    Implemented more of the ui by adding the welcome screen.

--- src/main/ui/RestaurantApp.java
+++ src/main/ui/RestaurantApp.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 // Represents the ui used for the application, 
 // can take in user input and respond to it
 public class RestaurantApp {
-    private boolean canRun = true;
-    private String currentCommand = null;
+    private boolean canRun;
+    private String currentCommand;
     private Scanner userInput;
 
     // EFFECTS: runs the Restaurant app
@@ -17,6 +17,10 @@ public class RestaurantApp {
     // EFFECTS: responsible for taking user input
     //          and running the display of the application
     private void runApp() {
+        canRun = true;
+        currentCommand = null;
+        userInput = new Scanner(System.in);
+        userInput.useDelimiter("\r?\n|\r");
 
         while (canRun) {
             welcomeScreen();
@@ -39,19 +43,35 @@ public class RestaurantApp {
     }
 
     // MODIFIES: this
-    // EFFECTS; finds the next command that user 
+    // EFFECTS: gets the next command that user 
     //          specifies
-    private String nextLine() {
+    private void nextLine() {
         currentCommand = userInput.next();
-        currentCommand.toLowerCase();
-        return currentCommand;
+        currentCommand = currentCommand.toLowerCase();
 
     }
 
     // EFFECTS: displays an opening message and
     //          shows commands user can input
     private void welcomeScreen() {
-        // stub
+        System.out.println("Welcome to Tim Horton's!");
+        line();
+        System.out.println("Please choose one of the following...");
+        line();
+        System.out.println("m = Menu");
+        System.out.println("p = Points I currently have");
+        System.out.println("a = Add an item to my cart");
+        System.out.println("r = Remove an item from my cart");
+        System.out.println("v = View items in my cart");
+        System.out.println("tp = Total points my cart is worth");
+        System.out.println("tm = Total price items in my cart are worth");
+        System.out.println("cp = Items in my cart purchasable with my current points");
+        System.out.println("bp = Buy items in my cart with my current points");
+        System.out.println("bm = Buy items in my cart with money");
+    }
+
+    private void line() {
+        System.out.println("-----------------------------------");
     }
 
 }
