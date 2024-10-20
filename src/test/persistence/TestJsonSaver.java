895d5a14ad90763d5e106993a3afb70ff3235b3e
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2024-10-19 22:31:09 -0700

    Finished writing the tests for the saver class, will begin its implementation now.

--- src/test/persistence/TestJsonSaver.java
+++ src/test/persistence/TestJsonSaver.java
@@ -7,7 +7,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 import model.Restaurant;
+import model.Food;
 
 // tests for JsonSaver class
 public class TestJsonSaver {
@@ -41,13 +44,45 @@ public class TestJsonSaver {
             assertEquals("Tim Horton's", readRestaurant.getName());
 
         } catch(IOException e) {
-            fail("Should not have caught exception here!");
+            fail("Should not have caught exception here - excecution should be fine!");
         }
     }
 
     @Test 
     void testWriteGeneralRestaurant() {
-        // TODO - finish this test
+        Restaurant restaurant = new Restaurant();
+        JsonSaver saver = new JsonSaver("./data/testWriteGeneralRestaurant.json");
+        restaurant.setUserPoints(15);
+
+        try {
+            Food f1 = new Food("Ice Capp", 10, 3.5, 5);
+            Food f2 = new Food("Coffee", 8, 2.5, 4);
+            Food f3 = new Food("Bagel", 5, 3, 2);
+            Food f4 = new Food("Tim Bits", 12, 5.5, 6);
+
+            saver.open();
+            saver.write(restaurant);
+            saver.close();
+
+            JsonLoader loader = new JsonLoader("./data/testWriteGeneralRestaurant.json");
+            Restaurant readRestaurant = loader.read();
+
+            assertEquals(15, readRestaurant.getUserPoints());
+            assertEquals("Tim Horton's", readRestaurant.getName());
+
+           ArrayList<Food> menu = readRestaurant.getMenuItems();
+
+           assertEquals(f1, menu.get(0));
+           assertEquals(f2, menu.get(1));
+           assertEquals(f3, menu.get(2));
+           assertEquals(f4, menu.get(3));
+           assertEquals(4, menu.size());
+
+        } catch (IOException e) {
+            fail("Should not have caught exception here - excecution should be fine!");
+        }
+        
+
     }
 
 }
